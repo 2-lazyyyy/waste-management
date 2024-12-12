624afa3a055e80ae069b4f152bb8df7091c97681
2-lazyyyy <138545404+2-lazyyyy@users.noreply.github.com>
2024-12-12 10:24:38 +0630

    Update dbConfig.jsx

--- utils/db/dbConfig.jsx
+++ utils/db/dbConfig.jsx
@@ -6,6 +6,6 @@ import { drizzle } from "drizzle-orm/neon-http";
 
 import * as schema from './schema' 
 
-const sql = neon('postgresql://neondb_owner:WN93UVmoJtKQ@ep-divine-voice-a1w4bksp.ap-southeast-1.aws.neon.tech/WasteManagementSystem?sslmode=require')
+const sql = neon(process.env.DATABASE_URL)
 
 export const db = drizzle(sql , {schema}) 
